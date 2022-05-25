db4ba44166c2c06dbeec80790c341f93c28feb5f
dev81log <dev81log@gmail.com>
2022-05-25 12:39:00 -0300

    Estudos sobre Data, iniciando repo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var data = new DateTime(2022, 05, 25, 12, 30, 10);
+//var data = DateTime.Now;
+
+Console.Clear();
+// Console.WriteLine(data);
+// Console.WriteLine(data.Year);
+// Console.WriteLine(data.Month);
+// Console.WriteLine(data.Day);
+// Console.WriteLine((int)data.DayOfWeek);
