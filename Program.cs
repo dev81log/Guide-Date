3953c25ba20da3d0864537eb3a780dbcc065506c
dev81log <dev81log@gmail.com>
2022-05-26 14:48:57 -0300

    Estudos de Date

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-var data = new DateTime(2022, 05, 25, 12, 30, 10);
-//var data = DateTime.Now;
+//var data = new DateTime(2022, 05, 25, 12, 30, 10);
+var data = DateTime.Now;
+
+var formatado = String.Format("{0:dd/MM/yyyy hh:mm:ss z}", data); // local
+var formatadoSystem = String.Format("{0:R}", data); // system
+var formatadoJson = String.Format("{0:s}", data); // json
+var formatadoUniversal = String.Format("{0:u}", data); // universal
+
+
 
 Console.Clear();
+Console.WriteLine(formatado);
+Console.WriteLine(formatadoSystem);
+Console.WriteLine(formatadoJson);
+Console.WriteLine(formatadoUniversal);
+
+
+
 // Console.WriteLine(data);
 // Console.WriteLine(data.Year);
 // Console.WriteLine(data.Month);
 // Console.WriteLine(data.Day);
 // Console.WriteLine((int)data.DayOfWeek);
+
