67167e859bf36fda041c0877a16744643043b694
dev81log <dev81log@gmail.com>
2022-05-28 21:33:04 -0300

    Atualização de estudos data

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿
+﻿//var data = new DateTime(2022, 05, 25, 12, 30, 10);
 
-//var data = new DateTime(2022, 05, 25, 12, 30, 10);
+// Console.WriteLine(data);
+// Console.WriteLine(data.Year);
+// Console.WriteLine(data.Month);
+// Console.WriteLine(data.Day);
+// Console.WriteLine((int)data.DayOfWeek);
+
+
 var data = DateTime.Now;
 
 var formatado = String.Format("{0:dd/MM/yyyy hh:mm:ss z}", data); // local
 var formatadoSystem = String.Format("{0:R}", data); // system
 var formatadoJson = String.Format("{0:s}", data); // json
 var formatadoUniversal = String.Format("{0:u}", data); // universal
-
 
 
 Console.Clear();
@@ -17,10 +22,15 @@ Console.WriteLine(formatadoJson);
 Console.WriteLine(formatadoUniversal);
 
 
+var dia = data.AddDays(10);
+var mes = data.AddMonths(1);
+var ano = data.AddYears(1);
 
-// Console.WriteLine(data);
-// Console.WriteLine(data.Year);
-// Console.WriteLine(data.Month);
-// Console.WriteLine(data.Day);
-// Console.WriteLine((int)data.DayOfWeek);
+Console.WriteLine(dia);
+Console.WriteLine(mes);
+Console.WriteLine(ano);
 
+if (data.Date == DateTime.Today)
+    Console.WriteLine("igual");
+    
+Console.WriteLine(data);
